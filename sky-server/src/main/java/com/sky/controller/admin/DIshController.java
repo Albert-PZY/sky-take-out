29b828b991c9cd2004a62bf9e25cd88d0d758580
Albert <1431686413@qq.com>
2025-05-01 15:18:45 +0800

    菜品分页查询业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DIshController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DIshController.java
@@ -2,17 +2,15 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
-import com.sky.entity.Dish;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -25,6 +23,7 @@ public class DIshController {
 
     /**
      * 新增菜品
+     *
      * @return Result
      */
     @ApiOperation("新增菜品")
@@ -33,6 +32,20 @@ public class DIshController {
         log.info("新增菜品{}", dishDTO);
         dishService.insertWithFlavors(dishDTO);
         return Result.success();
+    }
+
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return Result<PageResult>
+     */
+    @ApiOperation("菜品分页查询")
+    @GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO) {
+        log.info("菜品分页查询{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 
 }
