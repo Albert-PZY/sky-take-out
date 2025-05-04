c2de11221cda34a68fa313b31b2bc74d99b2430e
Albert <1431686413@qq.com>
2025-05-04 13:51:54 +0800

    管理端查看订单详情

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -6,11 +6,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +48,19 @@ public class OrderController {
         log.info("各状态订单统计");
         OrderStatisticsVO orderStatisticsVO = orderService.statistics();
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 查看订单详情
+     * @param id
+     * @return Result<OrderVO>
+     */
+    @ApiOperation("查看订单详情")
+    @GetMapping("/details/{id}")
+    public Result<OrderVO> details(@PathVariable Long id) {
+        log.info("查看订单详情");
+        OrderVO orderVO = orderService.details(id);
+        return Result.success(orderVO);
     }
 
 }
